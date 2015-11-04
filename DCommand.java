39ab9bcafe87522d8e4cf084c4228d072f052851
Pelt10 <pelt10@live.fr>
2015-11-04 14:59:30 +0100

    Initial Commit

---
+++ DCommand.java
@@ -0,0 +1,44 @@
+/*
+ * DynamicCommand
+ * @author Pelt10
+ * @version 1.0
+ */
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * @param name String with the command name
+ * @param usage String with the command syntax
+ * @param description String with the command description
+ * @param permission String with the command permission
+ * @param aliases An ArrayList with all alliases 
+ * @param executor The CommandExecutor of this command
+ * @param plugin The JavaPlugin of the Plugin who register this command
+ */
+public class DCommand extends BukkitCommand{
+	CommandExecutor executor;
+	protected DCommand(String name, String usage, String description,String permission, ArrayList<String> aliases, CommandExecutor executor, JavaPlugin plugin) {
+        super(name, description, usage, aliases);
+        this.setPermission(permission);
+        this.executor = executor;
+        
+        ((CraftServer) Bukkit.getServer()).getCommandMap().register(name, plugin.getName(), this);
+	}
+	
+	@Override
+	public boolean execute(CommandSender sender, String label, String[] args) {
+		if(executor != null)
+			executor.onCommand(sender, this, label, args);
+		return false;
+	}
+	
+	
+
+}
---
+++ LICENSE.md
@@ -0,0 +1,23 @@
+                     SKYOLICENSE
+               Version 2, August 2014
+
+ Copyright (C) 2014 Hugo DELAUNAY - “Skyost” - me@skyost.eu
+
+Everyone is permitted to use, copy and distribute copies of
+this license document, and changing it is allowed as long
+as the name is changed.
+
+The original version is available here :
+http://www.skyost.eu/Skyolicense.txt
+
+The official french version is available here :
+http://www.skyost.eu/Skyolicense.fr.txt
+
+TERMS AND CONDITIONS
+
+0. YOU ARE ALLOWED to view the source code of this software.
+1. YOU ARE ALLOWED to edit the source code WITHOUT giving credits as
+   personal use ONLY.
+2. YOU ARE ALLOWED to distribute a modified code from this software
+   BUT you MUST explicitly give credits in the code (as comment(s))
+   and on the software page (if exists). The source code must be accessible.
