0805f378b1721e9513ce79a0443a59964724b0db
Pelt10 <pelt10@live.fr>
2017-07-30 21:36:38 +0200

    Add new constructor

--- DCommand.java
+++ DCommand.java
@@ -1,44 +1,67 @@
-/*
- * DynamicCommand
- * @author Pelt10
- * @version 1.0
- */
+import java.util.List;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
-import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_9_R2.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/*
- * @param name String with the command name
- * @param usage String with the command syntax
- * @param description String with the command description
- * @param permission String with the command permission
- * @param aliases An ArrayList with all alliases 
- * @param executor The CommandExecutor of this command
- * @param plugin The JavaPlugin of the Plugin who register this command
+/**
+ * This class provide you to register a command without the plugin.yml and change the commandExecutor dynamically
+ * @author pelt10
+ * @version 1.1
  */
-public class DCommand extends BukkitCommand{
-	protected CommandExecutor executor;
-	
-	protected DCommand(String name, String usage, String description,String permission, ArrayList<String> aliases, CommandExecutor executor, JavaPlugin plugin) {
-            super(name, description, usage, aliases);
-            this.setPermission(permission);
-            this.executor = executor;
-        
-            ((CraftServer) Bukkit.getServer()).getCommandMap().register(name, plugin.getName(), this);
-	}
-	
-	@Override
-	public boolean execute(CommandSender sender, String label, String[] args) {
-		if(executor != null)
-	            return executor.onCommand(sender, this, label, args);
-	}
-	
-	
+public class DCommand extends BukkitCommand {
+    private CommandExecutor executor;
 
+    /**
+     * @param name command name
+     * @param executor command executor
+     * @param plugin {@link JavaPlugin} that own the command
+     */
+    public DCommand(String name, CommandExecutor executor, JavaPlugin plugin) {
+	super(name);
+	this.executor = executor;
+	registerCommand(plugin);
+    }
+    
+    /**
+     * @param name command name
+     * @param usage exemple usage
+     * @param description command description
+     * @param permission needed permission to start the command
+     * @param aliases list of aliases
+     * @param executor command executor
+     * @param plugin {@link JavaPlugin} that own the command
+     */
+    public DCommand(String name, String usage, String description, String permission, List<String> aliases, CommandExecutor executor, JavaPlugin plugin) {
+	super(name, description, usage, aliases);
+	this.setPermission(permission);
+	this.executor = executor;
+	registerCommand(plugin);
+    }
+
+    private void registerCommand(JavaPlugin plugin) {
+	((CraftServer) Bukkit.getServer()).getCommandMap().register(getName(), plugin.getName(), this);
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+	if (Objects.isNull(executor)) {
+	    throw new IllegalStateException("The executor for command " + getName() + " is null !");
+	}
+	return executor.onCommand(sender, this, label, args);
+    }
+    
+    /**
+     * You can change the {@link CommandExecutor} to change the behavior of the command over time.
+     * 
+     * @param executor new command Executor
+     */
+    public void setExecutor(CommandExecutor executor) {
+	this.executor = executor;
+    }
 }
